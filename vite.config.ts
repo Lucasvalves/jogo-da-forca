7d85bf1f0d790a4b552b731141962e6a5dfe5c43
Lucas Veloso <lucasvelosoalves@gmail.com>
2023-08-15 18:07:50 -0300

    finished

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jogo-da-forca',
+  base: '/jogo_da_forca',
   plugins: [react()],
 });
